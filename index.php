89aa0bc7a36d3c84d53020f28c05ffa0a5973ec7
Everton Lima <atevertonlima@gmail.com>
2014-06-04 16:58:49 -0300

    Projeto iniciado

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Passeio em Miami</title>
+        <link rel="stylesheet" href="assets/css/normalize.css">
+        <link rel="stylesheet" href="assets/css/foundation.min.css">
+        <link rel="stylesheet" href="assets/css/app.css">
+        <script src="assets/js/vendor/modernizr.js"></script>
+
+    </head>
+    <body>
+        <!-- Inicio do body -->
+        <img src="assets/img/bg.gif" class="fundoFakeBody" />
+        <div class="fakeBody">
+            <nav class="top-bar">
+                <div class="row small-8 columns small-offset-2">
+                    <ul class="title-area">
+                        <li class="name"><h1><a href="./"><img src="assets/img/logo-chapada.gif" /></a></h1></li>
+
+                        <!-- Mobile Menu Toggle -->
+                        <!--<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>-->
+                    </ul>
+                    
+                    <dl class="sub-nav right subnav-lineheght">
+                        <dd class="active"><a href="#">All</a></dd>
+                        <dd><a href="#">Active</a></dd>
+                        <dd><a href="#">Pending</a></dd>
+                        <dd><a href="#">Suspended</a></dd>
+                    </dl>
+                </div>
+            </nav>
+            <div class="row">
+
+            </div>            
+        </div>
+        <!-- Fim do body -->
+        <script src="assets/js/vendor/jquery.js"></script>
+        <script src="assets/js/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+</html>

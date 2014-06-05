66996f8b291d179f3723b20e04858c993f5f22ff
Everton Lima <atevertonlima@gmail.com>
2014-06-05 10:45:04 -0300

    Home iniciado

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <title>Passeio em Miami</title>
         <link rel="stylesheet" href="assets/css/normalize.css">
         <link rel="stylesheet" href="assets/css/foundation.min.css">
+        <link rel="stylesheet" href="assets/css/foundation-icons/foundation-icons.css">
         <link rel="stylesheet" href="assets/css/app.css">
         <script src="assets/js/vendor/modernizr.js"></script>
     </head>
@@ -15,15 +16,15 @@
         <img src="assets/img/bg.gif" class="fundoFakeBody" />
         <div class="fakeBody">
             <nav class="top-bar">
-                <div class="row small-8 medium-10 columns small-offset-2 medium-offset-1">
+                <div class="row medium-10 columns medium-offset-1">
                     <ul class="title-area">
                         <li class="name"><h1><a href="./"><img src="assets/img/logo-passeio-em-miami.png" /></a></h1></li>
                         <!-- Mobile Menu Toggle -->
-                        <!--<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>-->
+                        <li class="toggle-topbar menu-icon"><a href="#"><i class="fi-list-bullet large"></i></a></li>
                     </ul>
                     <!-- menu top-bar  -->
                     <dl class="sub-nav right subnav-lineheght">
-                        <dd class="active"><a href="#">Sobre</a></dd>
+                        <dd><a href="#">Sobre</a></dd>
                         <dd><a href="#">Imóveis</a></dd>
                         <dd><a href="#">Compras</a></dd>
                         <dd><a href="#">Enxoval</a></dd>
@@ -32,8 +33,61 @@
                 </div>
             </nav>
             <div class="row shadow bg-white">
-
-            </div>            
+                <ul class="example-orbit shadow" data-orbit>
+                    <li> 
+                        <img alt="Visão noturna de miami." src="assets/img/golfinhos.jpg" />
+                        <div class="orbit-caption"> Visão noturna de miami. </div> 
+                    </li>
+                    <li> 
+                        <img alt="Visão noturna de miami." src="assets/img/miami-noite.jpg" />
+                        <div class="orbit-caption"> Visão noturna de miami. </div> 
+                    </li>
+                    <li class="active"> 
+                        <img alt="Belo predio a beira-mar." src="assets/img/mansao-beira-mar.jpg" />
+                        <div class="orbit-caption"> Belo predio a beira-mar. </div> 
+                    </li> 
+                    <li> 
+                        <img alt="As praias de Miami." src="assets/img/praia-miami.jpg" /> 
+                        <div class="orbit-caption"> As praias de Miami. </div> 
+                    </li> 
+                </ul>
+                
+                <div class="row medium-10 columns ">
+                    <div class="medium-3 columns">
+                        <img src="http://lorempixel.com/400/300/animals" class="th radius">
+                        <h4>This is a content section.</h4>
+                        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+                    </div>
+                    <div class="medium-3 columns">
+                        <img src="http://lorempixel.com/400/300/city" class="th radius">
+                        <h4>This is a content section.</h4>
+                        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+                    </div>
+                    <div class="medium-3 columns">
+                        <img src="http://lorempixel.com/400/300/people" class="th radius">
+                        <h4>This is a content section.</h4>
+                        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+                    </div>
+                    <div class="medium-3 columns">
+                        <img src="http://lorempixel.com/400/300/transport" class="th radius">
+                        <h4>This is a content section.</h4>
+                        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+                    </div>
+                </div>
+            </div>
+            <footer class="top-bar">
+                <div class="large-6 columns">
+                    <p>&copy; Copyright no one at all. Go to town.</p>
+                </div>
+                <div class="large-6 columns">
+                    <ul class="inline-list right">
+                        <li><a href="#">Link 1</a></li>
+                        <li><a href="#">Link 2</a></li>
+                        <li><a href="#">Link 3</a></li>
+                        <li><a href="#">Link 4</a></li>
+                    </ul>
+                </div>
+            </footer>
         </div>
         <!-- Fim do body -->
         <script src="assets/js/vendor/jquery.js"></script>
